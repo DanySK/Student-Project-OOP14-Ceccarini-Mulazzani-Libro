f98c9e7eea802e1c7d8f5e771e1dbb698ba4b510
Alberto Mulazzani <a_mulazzani@yahoo.it>
2015-01-31 17:43:45 +0100

    Changed Internal division, improvement to MVC, fixes

--- src/mainview/MainClass.java
+++ src/mainview/MainClass.java
@@ -1,12 +1,14 @@
 package mainview;
 
+import view.MainGUI;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//new MainGUI();
-		new MagazGUI("BananaJoe");
+		new MainGUI();
+		//new MagazGUI("BananaJoe");
 	}
 
 }
