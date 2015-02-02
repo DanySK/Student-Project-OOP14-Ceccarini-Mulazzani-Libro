d7803d5fe7159087e4e8ec22414a49d4e1223662
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-02 18:13:18 +0100

    fix on statistics

--- src/model/Statistics.java
+++ src/model/Statistics.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 
 public class Statistics {
@@ -13,14 +15,20 @@ public class Statistics {
 		library.addAll(elenco.bookList());
 	}
 	
-	public List<Libro> mostPopular () {
+	public List<Libro> mostPopularBook () {
 		library.sort((b1,b2)->b2.getNSales() - b1.getNSales());
 		return library;
 	}
 	
-	public List<Libro> lessPopular () {
+	public List<Libro> lessPopularBook () {
 		library.sort((b1,b2)->b1.getNSales() - b2.getNSales());
 		return library;
+	}
+	
+	public Map <String, Integer> mostPopularAuthor () {
+		final Map <String, Integer> authors = new TreeMap <>();
+		
+		return authors;
 	}
 
 }
