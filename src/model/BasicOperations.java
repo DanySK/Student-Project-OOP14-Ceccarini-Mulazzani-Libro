e088df5d2f1db499b6b970472a4a3ae69bad718e
Alberto Mulazzani <alberto.mulazzani@studio.unibo.it>
2015-02-03 16:48:03 +0100

    Added BasicOperations class

---
+++ src/model/BasicOperations.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import exceptions.MissingBookException;
+
+public class BasicOperations implements IBasicOp{
+
+	protected Set<Libro> libreria = new HashSet<Libro>();
+	
+	
+	public void addBook(Libro book) {
+
+		if (!this.libreria.contains(book)) {
+			this.libreria.add(book);
+		} else {
+			this.libreria.forEach(b -> {
+				if (b.equals(book)) {
+					b.addCopy(book.getNCopy());
+				}
+			});
+		}
+	}
+
+
+	public void modifyBook (String title, String author, String[] fields) throws MissingBookException {
+		final Libro book = searchBook(title, author);
+		for (int i=0; i<fields.length; i++) {
+			if (!fields[i].isEmpty()) {
+				switch (i) {
+					case 0: book.setTitle(fields[i]);
+					case 1: book.setAuthor(fields[i]);
+					case 2: book.setYear(Integer.parseInt(fields[i]));
+					case 3: book.setEditor(fields[i]);
+					case 4: book.setISBN(fields[i]);
+					case 5: book.setPrice(Double.parseDouble(fields[i]));
+					case 6: book.setNCopy(Integer.parseInt(fields[i]));
+				}
+				
+			}
+		}
+		
+	}
+
+	@Override
+	public Libro searchBook(String title, String author)
+			throws MissingBookException {
+
+		for (Libro b:this.libreria) {
+			if (b.getTitle().equals(title) && b.getAuthor().equals(author)) {
+				return b;
+			}
+		}
+		throw new MissingBookException();	
+	}
+
+	@Override
+	public Set<Libro> bookList() {
+		// TODO Auto-generated method stub
+		return new HashSet<Libro>(libreria);	
+	}
+
+}
