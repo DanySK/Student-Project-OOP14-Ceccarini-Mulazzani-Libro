089331dd6a62e0b6531a97a54bc40ae53a10d6ac
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-03 16:27:20 +0100

    add test

--- src/model/Testordini.java
+++ src/model/Testordini.java
@@ -2,18 +2,45 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+
 import org.junit.Test;
+
+import exceptions.MissingBookException;
 
 public class Testordini {
 
 	@Test
 	public void test() {
 		IOrdini ord = new Ordini ();
-		 String fields [] = {"Titolo", "Autore", "1994", "Editore", "324", "32.5", "2" }  ;
-		 Libro book = new Libro (fields);
-		 ord.addBook(book);
-		 //ord.bookList();
-		 assertTrue(ord.equals(book));
+		IBookManagement lib = new BookManagement ();
+		String fields [] = {"Titolo", "Autore", "1994", "Editore", "324", "32.5", "2" }  ;
+		Libro book = new Libro (fields);
+		HashSet <Libro> set = new HashSet <>();
+		 
+		set.add(book);
+		ord.addBook(book);
+		 
+		assertTrue(ord.bookList().equals(set));
+		assertTrue(ord.bookList().size() == 1);
+		assertTrue(lib.bookList().size() == 0);
+		assertFalse(lib.bookList().equals(set));
+		
+		lib.addBook(book);
+		try {
+			lib.sellBook(book);
+		} catch (MissingBookException e) {
+			e.printStackTrace();
+		}
+		
+		assertTrue(lib.bookList().equals(set));
+		
+		ord.evasioneOrdini();
+		//assertTrue(ord.bookList().isEmpty());
+		assertEquals(ord.bookList().size(), 0);
+		//assertFalse(ord.bookList().equals(set));
+		
+		
 	}
 
 }
