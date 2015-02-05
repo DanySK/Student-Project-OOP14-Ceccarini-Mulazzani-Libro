f06a2047e8291b9e15b9612c6f90d14ae311a458
Alberto Mulazzani <alberto.mulazzani@studio.unibo.it>
2015-02-05 13:05:53 +0100

    Added BookDetailGUI, ready to use #WeStillLoveGuis

---
+++ src/view/BookDetailGUI.java
@@ -0,0 +1,51 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Libro;
+import utilities.GUIUtilities;
+
+public class BookDetailGUI extends JDialog{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5294910808056208082L;
+	private final JPanel main = new JPanel();
+	private final String[] names = {"Titolo", "Autore", "Anno di pubblicazione", "Editore", "ISBN", "Prezzo", "Quantità", "Copie Vendute" };
+	
+	
+	public BookDetailGUI(Libro b){
+		
+		main.setLayout(new BorderLayout());
+		
+		final JPanel mid = new JPanel(new GridLayout(0,2));
+		
+		String[] fields = b.getFields();
+		
+
+		for (int i = 0; i < fields.length; i++){
+			mid.add(GUIUtilities.wrapperPanel(new JLabel(names[i]),FlowLayout.RIGHT));
+			mid.add(GUIUtilities.wrapperPanel(new JLabel(fields[i]),FlowLayout.LEFT));
+		}
+		
+		
+		main.add(mid, BorderLayout.CENTER);
+		
+		
+	}
+	
+	public JPanel getPane(){
+		return this.main;
+	}
+	
+	
+	
+}
