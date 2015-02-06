0929b014aae79e91b890231628a3b1f0b2c1d438
Alberto Mulazzani <alberto.mulazzani@studio.unibo.it>
2015-02-06 15:52:57 +0100

    Added modify guis, ready 100%

---
+++ src/view/DetailsToModifyGUI.java
@@ -0,0 +1,90 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import model.Libro;
+import utilities.GUIUtilities;
+import controller.BookController;
+import exceptions.WrongDataException;
+
+public class DetailsToModifyGUI	extends JDialog {
+	
+	
+	private static final long serialVersionUID = -5294910808056208082L;
+	private final JPanel main = new JPanel();
+	private final String[] names = {"Titolo", "Autore", "Anno di pubblicazione", "Editore", "ISBN", "Prezzo", "Quantità", "Copie Vendute" };
+	private final JTextField[] jfields = new JTextField[names.length];
+	private final JButton conf = new JButton("Conferma");
+	
+	
+	
+	public DetailsToModifyGUI(BookController controller, Libro b){
+		
+		main.setLayout(new BorderLayout());
+		
+		final JPanel mid = new JPanel(new GridLayout(0,3));
+		
+		String[] fields = b.getFields();
+		
+		
+		
+
+		for (int i = 0; i < fields.length; i++){
+			jfields[i] = new JTextField(15);
+			mid.add(GUIUtilities.wrapperPanel(new JLabel(names[i]),FlowLayout.RIGHT));
+			mid.add(GUIUtilities.wrapperPanel(new JLabel(fields[i]),FlowLayout.LEFT));
+			mid.add(GUIUtilities.wrapperPanel(jfields[i], FlowLayout.RIGHT));
+		}
+		
+		
+		main.add(mid, BorderLayout.CENTER);
+		
+		
+		final JPanel bot = new JPanel(new FlowLayout());
+		
+		bot.add(conf);
+		bot.add(GUIUtilities.getReset(jfields));
+		
+		
+		main.add(bot, BorderLayout.SOUTH);
+		
+		conf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					controller.modifyBook(b, jfields);
+					JOptionPane.showMessageDialog(main, "Il libro è stato modificato correttamente", "Successo!", JOptionPane.INFORMATION_MESSAGE);
+					controller.addBook();
+					JOptionPane optionPane = (JOptionPane)
+						    SwingUtilities.getAncestorOfClass(JOptionPane.class, conf);
+						optionPane.setValue(JOptionPane.CLOSED_OPTION);
+				} catch (WrongDataException e1) {
+					
+					JOptionPane.showMessageDialog(main, "I dati inseriti non sono corretti, ricontrollare", "Dati Errati", JOptionPane.ERROR_MESSAGE);				
+				}
+				
+			}
+		});
+		
+		
+		
+	}
+	
+	public JPanel getPane(){
+		return this.main;
+	}
+
+}
---
+++ src/view/ModifyBookGUI.java
@@ -0,0 +1,79 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import controller.BookController;
+import utilities.GUIUtilities;
+import exceptions.MissingBookException;
+
+public class ModifyBookGUI {
+	
+	
+	
+	private JPanel main = new JPanel();
+	private JButton conf = new JButton("Conferma");
+	private JTextField[] fields = new JTextField[2];
+	private String[] names = {"Titolo", "Autore",};
+	
+	public ModifyBookGUI(BookController controller){
+		
+		main.setLayout(new BorderLayout());
+		
+		final JPanel mid = new JPanel(new GridLayout(0,2));
+		
+		
+		for (int i = 0; i < fields.length; i++ ){
+			fields[i] = new JTextField(15);
+		}
+		
+		for (int i = 0; i < fields.length; i++){
+			mid.add(GUIUtilities.wrapperPanel(new JLabel(names[i]),FlowLayout.RIGHT));
+			mid.add(GUIUtilities.wrapperPanel(fields[i],FlowLayout.CENTER));
+		}
+		
+		
+		main.add(mid, BorderLayout.CENTER);
+		
+		final JPanel bot = new JPanel(new FlowLayout());
+		
+		bot.add(conf);
+		bot.add(GUIUtilities.getReset(fields));
+		main.add(bot, BorderLayout.SOUTH);
+		
+		
+		conf.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					JOptionPane.showOptionDialog(main, new DetailsToModifyGUI(controller, controller.searchBook(fields)).getPane() , "Modifica libro", JOptionPane.DEFAULT_OPTION, 
+					         JOptionPane.INFORMATION_MESSAGE, GUIUtilities.icon, GUIUtilities.string, null);
+					controller.addBook();
+					JOptionPane optionPane = (JOptionPane)
+						    SwingUtilities.getAncestorOfClass(JOptionPane.class, conf);
+						optionPane.setValue(JOptionPane.CLOSED_OPTION);
+				} catch (MissingBookException e) {
+					
+					JOptionPane.showMessageDialog(main, "Il libro non è presente", "Libro mancante", JOptionPane.ERROR_MESSAGE);							
+				}
+			}
+		});
+	}
+	
+	public JPanel getPane(){
+		return this.main;
+	}
+	
+
+}
