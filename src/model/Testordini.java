1611413ae5493a59405804d832688fc463cc461f
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-10 16:17:33 +0100

    changes to test

--- src/model/Testordini.java
+++ src/model/Testordini.java
@@ -14,33 +14,44 @@ public class Testordini {
 
 	@Test
 	public void test() {
+		//creo la lista degli ordini
 		IOrdini ord = new Ordini ();
-
+		//creo la libreria
 		IBookManagement lib = new BookManagement ();
 		String fields [] = {"Titolo", "Autore", "1994", "Editore", "324", "32.5", "2" }  ;
+		//creo un nuovo libro
 		Libro book = new Libro (fields);
 		List <Libro> list = new ArrayList <>();
-		 
+		//inserisco il libro nella lista di prova
 		list.add(book);
+		//inserisco il libro negli ordini
 		ord.addBook(book);
 		 
+		//controllo che nella lista degli ordini sia presente solo il libro appena inserito
 		assertTrue(ord.bookList().equals(list));
+		//controllo quindi che la sua size sia 1
 		assertTrue(ord.bookList().size() == 1);
+		//controllo che nella libreria inizialmente non ci sia alcun libro
 		assertTrue(lib.bookList().size() == 0);
+		//controllo che il libro non sia presente nella libreria
 		assertFalse(lib.bookList().equals(list));
 		
+		//aggiungo il libro alla libreria
 		lib.addBook(book);
+		//se il libro è presente, ne vendo due copie
 		try {
 			lib.sellBook(book, 2);
 		} catch (MissingBookException | NotEnoughBookException e) {
 			e.printStackTrace();
 		} 
-		
+		//controllo che nella libreria rimanga sempre quel libro (cambieranno le copie presenti)
 		assertTrue(lib.bookList().equals(list));
 		
+		//evado gli ordini
 		ord.evasioneOrdini();
+		//controllo che non ci siano più ordini
 		assertTrue(ord.bookList().isEmpty());
-		assertEquals(ord.bookList().size(), 0);
+		//controllo che non ci sia più il libro
 		assertFalse(ord.bookList().equals(list));
 		
 
