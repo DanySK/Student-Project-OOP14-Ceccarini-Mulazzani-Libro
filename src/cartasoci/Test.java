c885692b6d85c29f04abd7dc56c379d9eec18c69
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-11 13:46:56 +0100

    Test

--- src/cartasoci/Test.java
+++ src/cartasoci/Test.java
@@ -1,12 +1,5 @@
 package cartasoci;
 
-import view.AddPersonGUI;
-
 public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//new AddPersonGUI();
-	}
 
 }
