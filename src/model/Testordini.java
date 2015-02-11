f233973157a044b065226286409278c24679d9d4
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-11 12:55:20 +0100

    add TestOrdini

--- src/model/Testordini.java
+++ src/model/Testordini.java
@@ -14,9 +14,12 @@ public class Testordini {
 		//creo la lista degli ordini
 		IOrdini ord = new Ordini ();
 
-		String fields [] = {"Titolo", "Autore", "1994", "Editore", "324", "32.5", "2" }  ;
+		String fields [] = {"Titolo", "Autore", "1994", "Editore", "324", "32.5", "2" };
+		String fields2 [] = {"", "", "1995", "", "", "14.7", "7" };
+		
 		//creo un nuovo libro
 		Libro book = new Libro (fields);
+		Libro bprova = new Libro (fields);
 		
 		List <Libro> list = new ArrayList <>();
 		
@@ -30,6 +33,12 @@ public class Testordini {
 		//controllo quindi che la sua size sia 1
 		assertTrue(ord.bookList().size() == 1);
 		
+		//modifico i campi del libro all'interno degli ordini
+		ord.modifyBook(book, fields2);
+		//ho modificato l'anno, che è quindi diverso dal libro con i fields iniziali
+		assertFalse (ord.bookList().get(0).getYear() == bprova.getYear());
+		//cambiando il libro all'interno di ord cambio anche il libro di partenza
+		assertTrue (ord.bookList().get(0).getYear() == book.getYear());
 		
 		//evado gli ordini
 		ord.evasioneOrdini();
@@ -37,6 +46,7 @@ public class Testordini {
 		assertTrue(ord.bookList().isEmpty());
 		//controllo che non ci sia più il libro
 		assertFalse(ord.bookList().equals(list));
+		
 		
 
 	}
