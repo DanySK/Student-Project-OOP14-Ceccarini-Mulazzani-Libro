e47b73195c60f1694a14268a617afefb37d29973
Chiara Ceccarini <chiara.ceccarini5@studio.unibo.it>
2015-02-13 16:45:17 +0100

    fix

--- src/model/IBookManagement.java
+++ src/model/IBookManagement.java
@@ -15,26 +15,26 @@ public interface IBookManagement extends IBasicOp{
 	
 	/**
 	 * 
-	 * @param book
-	 * @param nCopy
-	 * @throws MissingBookException
-	 * @throws NotEnoughBookException
+	 * @param book to sell
+	 * @param nCopy to sell
+	 * @throws MissingBookException if book is not present
+	 * @throws NotEnoughBookException if there aren't enough copy
 	 */
 	void sellBook(Libro book, int nCopy) throws MissingBookException, NotEnoughBookException;
 	
 	/**
 	 * 
-	 * @param title
+	 * @param title of book
 	 * @return book searched by title
-	 * @throws MissingBookException
+	 * @throws MissingBookException if the book is not present
 	 */
 	List <Libro> searchBookTitle(String title) throws MissingBookException;
 	
 	/**
 	 * 
-	 * @param author
+	 * @param author of the book
 	 * @return book searched by author
-	 * @throws MissingBookException
+	 * @throws MissingBookException if the book is not present
 	 */
 	List<Libro> searchBookAuthor (String author) throws MissingBookException;
 
